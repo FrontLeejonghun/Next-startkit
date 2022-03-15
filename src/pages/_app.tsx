28557eba1ed14a93f3a84323f30b5378a5cbf6d9
FrontLeejonghun <leejonghun32425@gmail.com>
2022-03-15 11:30:27 +0900

    Update - 각종 파일 업데이트

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,11 @@
-import { useEffect, useState } from 'react';
 import { AppProps } from 'next/app';
-import dynamic from 'next/dynamic';
 import { RecoilRoot } from 'recoil';
+
 import 'styles/global.scss';
 
 function App({ Component, pageProps }: AppProps) {
-  const [root, setRoot] = useState<HTMLElement>();
-
-  const RecoilizeDebugger = dynamic(
-    () => {
-      return import('react-recoilize');
-    },
-    { ssr: false },
-  );
-
-  useEffect(() => {
-    if (typeof window.document !== 'undefined') {
-      setRoot(document.getElementById('__next') as HTMLElement);
-    }
-  }, [root]);
   return (
     <RecoilRoot>
-      <RecoilizeDebugger root={root} />
       <Component {...pageProps} />
     </RecoilRoot>
   );
