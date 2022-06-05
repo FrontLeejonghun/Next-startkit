31f13c528cd815f89c8cdd81c652b8cc58729982
FrontLeejonghun <leejonghun32425@gmail.com>
2022-06-05 19:12:41 +0900

    update: storybook setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { NextPage } from 'next';
+import { SampleButton } from 'components';
 
 const Home: NextPage = () => {
   return (
     <>
       <div>next Start kit</div>
+      <SampleButton label="test" />
     </>
   );
 };
