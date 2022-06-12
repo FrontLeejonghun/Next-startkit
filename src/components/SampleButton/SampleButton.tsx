76abcd64e41644fc20dedad545ee83a5a8d26036
FrontJonghun <leejonghun3245@gmail.com>
2022-06-12 13:46:14 +0900

    Revert "ㄴㅁㅇㄴㅁㅇ"
    This reverts commit c4fee9bdde85a59097fc034372d27f023643bbfe.

--- src/components/SampleButton/SampleButton.tsx
+++ src/components/SampleButton/SampleButton.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import classNames from 'classnames/bind';
-import Image from 'next/image';
 import styles from './SampleButton.module.scss';
 
 const cx = classNames.bind(styles);
@@ -12,16 +11,9 @@ interface ButtonProps {
 }
 
 export const SampleButton = ({ type = 'primary', label, ...rest }: ButtonProps) => {
-  const [state, setState] = useState('dd');
-  if (state) {
-    useEffect(() => {}, []);
-  }
   return (
-    <>
-      <button type="button" className={cx('button', type)} {...rest}>
-        {label}
-      </button>
-      <Image src="/vercel.svg" width={72} height={16} />
-    </>
+    <button type="button" className={cx('button', type)} {...rest}>
+      {label}
+    </button>
   );
 };
