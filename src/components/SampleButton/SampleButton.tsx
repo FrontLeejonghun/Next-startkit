c4fee9bdde85a59097fc034372d27f023643bbfe
FrontJonghun <leejonghun3245@gmail.com>
2022-06-12 13:46:14 +0900

    ㄴㅁㅇㄴㅁㅇ

--- src/components/SampleButton/SampleButton.tsx
+++ src/components/SampleButton/SampleButton.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
+import Image from 'next/image';
 import styles from './SampleButton.module.scss';
 
 const cx = classNames.bind(styles);
@@ -11,9 +12,16 @@ interface ButtonProps {
 }
 
 export const SampleButton = ({ type = 'primary', label, ...rest }: ButtonProps) => {
+  const [state, setState] = useState('dd');
+  if (state) {
+    useEffect(() => {}, []);
+  }
   return (
-    <button type="button" className={cx('button', type)} {...rest}>
-      {label}
-    </button>
+    <>
+      <button type="button" className={cx('button', type)} {...rest}>
+        {label}
+      </button>
+      <Image src="/vercel.svg" width={72} height={16} />
+    </>
   );
 };
